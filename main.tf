1049abfe59ae1cd9044774c9a973f334890d14f5
Aleksandr Frolov <owirtifer@yandex.ru>
2021-12-03 14:45:55 +0300

    Add module EC2 Instance

--- main.tf
+++ main.tf
@@ -90,13 +90,45 @@ resource "aws_network_interface" "ni_test" {
   }
 }
 
-# Create Instance
-resource "aws_instance" "netology" {
+## Create Instance
+#resource "aws_instance" "netology" {
+#  ami                    = data.aws_ami.ubuntu.id
+#  instance_type          = "t2.micro"
+##  vpc_security_group_ids = [aws_vpc.vpc-test.default_security_group_id]
+##  subnet_id              = aws_subnet.subnet_test.id
+##  security_groups        = [aws_security_group.sg-test.id]
+#
+#  network_interface {
+#    network_interface_id = aws_network_interface.ni_test.id
+#    device_index         = 0
+#  }
+#
+#  root_block_device {
+#    delete_on_termination = true
+#    #iops                  = 3000
+#    tags                  = {
+#      Name = "Test EBS Volume"
+#    }
+#    #throughput            = 125
+#    volume_size           = 8
+#    volume_type           = "gp2"
+#  }
+#
+#  tags = {
+#    Name = "Netology"
+#  }
+#}
+
+# Using AWS EC2 Instance Terraform module
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "netology"
+
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
-#  vpc_security_group_ids = [aws_vpc.vpc-test.default_security_group_id]
-#  subnet_id              = aws_subnet.subnet_test.id
-#  security_groups        = [aws_security_group.sg-test.id]
 
   network_interface {
     network_interface_id = aws_network_interface.ni_test.id
@@ -105,11 +137,9 @@ resource "aws_instance" "netology" {
 
   root_block_device {
     delete_on_termination = true
-    #iops                  = 3000
     tags                  = {
       Name = "Test EBS Volume"
     }
-    #throughput            = 125
     volume_size           = 8
     volume_type           = "gp2"
   }
