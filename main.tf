f64619587c0c3c62ce933567c7924fea415fbc64
Aleksandr Frolov <owirtifer@yandex.ru>
2021-12-03 15:48:57 +0300

    Add instance

--- main.tf
+++ main.tf
@@ -130,6 +130,17 @@ module "ec2_instance" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
 
+  network_interface = [aws_network_interface.ni_test.id]
+
+  root_block_device = {
+    delete_on_termination = true
+    tags                  = {
+      Name = "Test EBS Volume"
+    }
+    volume_size           = 8
+    volume_type           = "gp2"
+  }
+
   tags = {
     Name = "Netology"
   }
