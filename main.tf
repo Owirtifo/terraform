72a7a352285e522dd9bcbd44873d5378eab63bb5
Aleksandr Frolov <owirtifer@yandex.ru>
2021-12-03 16:04:46 +0300

    Add instance

--- main.tf
+++ main.tf
@@ -130,7 +130,7 @@ module "ec2_instance" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 
-  network_interface = [aws_network_interface.ni_test.id]
+#  network_interface = [aws_network_interface.ni_test.id]
 
 #  root_block_device = [
 #    {
