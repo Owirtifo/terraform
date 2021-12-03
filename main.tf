70221d14b72beca31ba09c03fccc9a44c7bea461
Aleksandr Frolov <owirtifer@yandex.ru>
2021-12-03 16:14:57 +0300

    Add instance

--- main.tf
+++ main.tf
@@ -130,17 +130,20 @@ module "ec2_instance" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 
-#  network_interface = [aws_network_interface.ni_test.id]
+  network_interface = [{
+    network_interface_id = aws_network_interface.ni_test.id
+    device_index         = 0
+  }]
 
-#  root_block_device = [
-#    {
-#      delete_on_termination = true
-#      tags                  = {
-#        Name = "Test EBS Volume"
-#      }
-#      volume_size           = 8
-#      volume_type           = "gp2"
-#    }]
+  root_block_device = [
+    {
+      delete_on_termination = true
+      tags                  = {
+        Name = "Test EBS Volume"
+      }
+      volume_size           = 8
+      volume_type           = "gp2"
+    }]
 
   tags = {
     Name = "Netology"
